20613520db337806b8401b1eaffdd54aa511af91
rxbinashley <40203570+rxbinashley@users.noreply.github.com>
2021-01-09 21:08:14 +0100

    commit

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -743,9 +743,9 @@ var checkedAssets, assetsToAccept;
 var parent = module.bundle.parent;
 
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = "" || location.hostname;
+  var hostname = "0.0.0.0" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59561" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57392" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
